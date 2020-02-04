8126ee051a99f23c30a8ab9b8ac1e69cf3927fc8
Ankur Tripathi <ankur.2323@gmail.com>
2020-02-04 23:52:21 +0530

    added chalk and validator and nodemon

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const validator = require('validator')
+const chalk = require('chalk')
 const getNotes = require('./notes.js');
 
 console.log(getNotes());
 
 console.log(validator.isEmail('sdkjcdsb@sdfljkvnd.com'));
+
+console.log(chalk.red.bold('Error!!'));
